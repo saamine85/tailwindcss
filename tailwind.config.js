cced72453e659803f29d5af2b1227d6e2f2b80a0
amine sadi <aminsba44@gmail.com>
2022-05-18 08:44:12 +0100

    taiwind commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        open: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        gradient1: "#6366f1",
+        gradient2: "#d8b4fe",
+        gradient3: "#ec4899",
+        fillprimary: "#181818",
+      },
+    },
+  },
+  plugins: [],
+};
